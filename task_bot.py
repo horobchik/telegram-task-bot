f83c4f642a4334f7c59447f9cb2d8b5869337c6a
horobchik <serhiiharapko@gmail.com>
2025-04-10 09:56:34 +0300

    Update task_bot.py

--- task_bot.py
+++ task_bot.py
@@ -151,8 +151,9 @@ async def send_all_tasks(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         status = user_data[user_id]['statuses'][i]
         await context.bot.send_message(
             chat_id=user_id,
-            text=f"📌 Завдання {i+1}:\n{status}{task}",
-            reply_markup=InlineKeyboardMarkup(keyboard)
+            text=f"📌 <b>Завдання {i+1}:\n{status}{task}<b>",
+            reply_markup=InlineKeyboardMarkup(keyboard),
+    parse_mode="HTML"
         )
 
 async def handle_button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
